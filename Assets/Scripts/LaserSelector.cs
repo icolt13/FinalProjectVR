82f5b0485353675fec555bf3b02ff07183b7bff9
krueg569 <krueg569@uwm.edu>
2023-12-19 23:02:33 -0600

    Update Keyboard
    Functioning Keyboard with following issues
    - Trigger click spams keys
    - Input textbox limits visible characters

--- Assets/Scripts/LaserSelector.cs
+++ Assets/Scripts/LaserSelector.cs
@@ -1,55 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class LaserSelector : MonoBehaviour
 {
     public Material laserMaterial;
-    public Material indicationMaterial;
-
-    private LineRenderer lineRenderer;
-    private GameObject indicatedGameObject = null;
-    private Material tmpMaterial;
-
+    public LineRenderer lineRenderer;
+    public float raycastDistance = 10f;
     public enum Hand { LEFT, RIGHT };
     public Hand hand = Hand.RIGHT;
 
     // Start is called before the first frame update
     void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.material = laserMaterial;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 rayEnd = transform.position + transform.forward * 100f;
+        //Update Line Renderer Position
+        Ray ray = new Ray(transform.position, transform.forward);
+        lineRenderer.SetPosition(0, transform.position);
+        Vector3 endPosition = transform.position + transform.forward * raycastDistance;
+        lineRenderer.SetPosition(1, endPosition);
 
+        // Raycast to detect UI elements
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        if (Physics.Raycast(ray, out hit, raycastDistance))
         {
-            rayEnd = hit.point;
-
+            // Check if the hit object is a UI element
             if (hit.collider.tag == "Pressable")
             {
-                Renderer renderer = hit.collider.gameObject.GetComponent<Renderer>();
-
-                if (indicatedGameObject == null)
+                if ((hand == Hand.RIGHT && OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger) > 0.9f) ||
+                    (hand == Hand.LEFT && OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger) > 0.9f))
                 {
-                    tmpMaterial = renderer.material;
-                    renderer.material = indicationMaterial;
-                    indicatedGameObject = hit.collider.gameObject;
+                    hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
                 }
             }
         }
-        else if (indicatedGameObject != null)
-        {
-            indicatedGameObject.GetComponent<Renderer>().material = tmpMaterial;
-            indicatedGameObject = null;
-        }
-
-        lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, rayEnd);
     }
 }
